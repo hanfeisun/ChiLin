c3df63dc47e21d2d176f611550656215e6c0bff4
shenglin mei <samleomei@gmail.com>
2012-07-04 10:48:39 +0800

    QC pipeline framework, you can just execute the QCreport function at lib directory

---
+++ QCreport/build/lib/QCreport/Basic_qc.py
@@ -0,0 +1,3 @@
+
+def Basic_info():
+	print 'Basic_info():  return basic information about the dataset. such as datasetid, species, factor, replicate, raw data format, and so on.'
---
+++ QCreport/lib/Conservtion_qc.py
@@ -0,0 +1,3 @@
+
+def conservation_info():
+	print '''conservation_info()   return conservation graph'''
