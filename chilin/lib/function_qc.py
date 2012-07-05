f7ce58543b3890124129cf1c44b32a5aa12b8a19
shenglin mei <samleomei@gmail.com>
2012-07-05 21:34:17 +0800

    add latex templatie and fix the frame work

---
+++ chilin/lib/function_qc.py
@@ -0,0 +1,13 @@
+
+def Ceas_info():
+	print '''
+ceas
+
+	'''
+def Conservation_info():
+	print '''conservation_info()   return conservation graph'''
+
+
+
+def Motif_info():
+	print 'motif_info()  return motif information'
