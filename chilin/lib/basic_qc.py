e16771c589442acfdd2e7ffa8a943d7d3d688747
shenglin mei <samleomei@gmail.com>
2012-07-09 20:47:45 +0800

    add template.tex and QC class

--- chilin/lib/basic_qc.py
+++
@@ -1,8 +0,0 @@
-
-def Basic_info():
-	print '''Basic_info():  return basic information about the dataset. such as datasetid, species, factor,     replicate, raw data format, and so on.'
-'''
-
-def fastqc_info():
-	print '''fastqc_info():  if the raw data format is fastq. this part will execute and return the summary list of fastqc result and the figure of score distribution.
-	'''
