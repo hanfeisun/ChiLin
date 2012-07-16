dc0255beade6332c55cbfa0d10155fb717c8bd79
Hanfei Sun <hfsun.tju@gmail.com>
2012-07-15 22:57:50 -0400

    add dc_runner and improve qc_runner

--- chilin/tests/qc_runner.py
+++ chilin/tests/qc_runner.py
@@ -5,15 +5,29 @@ from chilin.qc import (
 	AnnotationQC)
 import unittest
 
-
-
-
 class RawQCTestCase(unittest.TestCase):
     def setUp(self):
         self.rawqc = RawQC()
     def testRun(self):
         self.rawqc.run()
 
+class MappingQCTestCase(unittest.TestCase):
+    def setUp(self):
+        self.mappingqc = MappingQC()
+    def testRun(self):
+        self.mappingqc.run()
+        
+class PeakcallingQCTestCase(unittest.TestCase):
+    def setUp(self):
+        self.peakcallingqc = PeakcallingQC()
+    def testRun(self):
+        self.peakcallingqc.run()
+
+class AnnotationQCTestCase(unittest.TestCase):
+    def setUp(self):
+        self.annotationqc = AnnotationQC()
+    def testRun(self):
+        self.annotationqc.run()
 
 
 if __name__ == '__main__':
