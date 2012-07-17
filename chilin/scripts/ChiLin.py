db7bfa1f48d3e3b133ee39e6e38b74ffa61fec04
Alvin_Qin <qinqianhappy@gmail.com>
2012-07-17 20:40:42 +0800

    dc.py class name change, so does api.rst

--- chilin/scripts/ChiLin.py
+++ chilin/scripts/ChiLin.py
@@ -2,33 +2,30 @@
 """
 Main program of DC pipeline
 """
-import sys
-sys.path.append("../lib/")
-from dc import *
-from qc import *
+from chilin.dc import *
+from chilin.qc import *
 from optparse import OptionParser
 
 def main():
-    usage = "usage: %prog <Meta.xls Path> [optional]-m cormethod -p peaksnumber"
+    usage = "usage: %prog <ChiLin.conf Path> [optional]-m cormethod -p peaksnumber"
     description = "ChiLin : A clear ChIP-seq pipeline"
     parser = OptionParser(version = "%prog 1.0.0", description = description, usage = usage)
     parser.add_option("-m", dest = "cormethod", type = "string", default = "mean",
                       help = "specify method for correlation plot")
     parser.add_option("-p", dest = "peaksnumber", type = "int", default = 3000,
                       help = "specify peaks number for CEAS, Conservation and options")
+    parser.add_option("-t", dest = "type", type = "string", 
+                      help = "specify the analysis type, supported Dnase, Histone, TF")
     (options, args) = parser.parse_args()
 
-    if not args:
+    if not args or not options.type:
         parser.print_help()
         sys.exit(1)
-    Meta = args[0]
-    Config = Check().ReadConf()
+    ChiLinConf = args[0]
+    Config = Check().ReadConf(ChiLinConf)
+
     print Config
 #    ['bowtie', 'samtools', 'macs', 'bedtools', 'bed2bam', 'ceas', 'conservation', 'correlation', 'venn', 'seqpos', 'QC']
-
-    
-    Meta = Check().MetaParse(Meta)
-    
 
     print "test OK"
 
