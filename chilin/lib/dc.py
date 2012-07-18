cac72ad52576b9693c4c0fb412e239bd74e87c7c
Alvin_Qin <qinqianhappy@gmail.com>
2012-07-18 16:25:09 +0800

    merge and add fastq test file

--- chilin/lib/dc.py
+++ chilin/lib/dc.py
@@ -8,18 +8,6 @@ import glob
 import datetime
 import os
 
-jinja_env = Environment(loader = PackageLoader('chilin', 'template'),
-                        block_start_string = '\BLOCK{',
-                        block_end_string = '}',
-                        variable_start_string = '\VAR{',
-                        variable_end_string = '}',
-                        comment_start_string = '\#{',
-                        comment_end_string = '}',
-                        line_statement_prefix = '%-',
-                        line_comment_prefix = '%#',
-                        trim_blocks = True,
-                        autoescape = False,
-                        )
 cf = ConfigParser()
 safecf = SafeConfigParser()
 class PipePreparation:
