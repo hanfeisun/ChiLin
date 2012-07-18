4705453a4dc8184bd5c644a4d3257aba110162be
Alvin_Qin <qinqianhappy@gmail.com>
2012-07-18 16:08:56 +0800

    add fastq test file

---
+++ chilin/lib/qc.py
@@ -0,0 +1,170 @@
+from chilin.dc import *
+
+class QC_Controller(object):
+	"""
+	All the class in the module derives from this class
+	"""
+	def __init__(self):
+#		self.pathfinder = PathFinder(conf)
+		self.env = Environment(
+				loader=FileSystemLoader('/Users/Samleo/mybin/chilin/chilin/lib/template/'),
+				block_start_string = '\BLOCK{',
+				block_end_string = '}',
+				variable_start_string = '\VAR{',
+				variable_end_string = '}',
+				comment_start_string = '\#{',
+				comment_end_string = '}',
+				line_statement_prefix = '%-',
+				line_comment_prefix = '%#',
+				trim_blocks = True,
+				autoescape = False,
+				)
+		print 'pass'
+		self.template = self.env.get_template('template.tex')
+		self.has_run = False
+		print 'QC control'
+	
+	def run(self):
+		""" Run some QC tools or do some time-costing statistics """
+		self.has_run = True
+		return True
+
+	def _check(self):
+		""" Check whether the quality of the dataset is ok. """
+		if not self.has_run:
+			self.run()
+		return True
+
+	def _render(self, template = None):
+		""" Generate the latex code for current section. """
+#		self.template.render({})
+		pass
+		
+
+class RawQC(QC_Controller):
+	"""  
+	RawQC aims to perform some simple quality control checks to ensure that the raw data looks good and there are no problems or biases in your data.
+	"""
+	def __init__(self):
+		super(RawQC, self).__init__()
+		self.has_fastqc = False
+		self.basic_stat = ''
+		self.fastqc_summary_stat = ''
+		fastqc_graph_stat = ''
+		print 'init basic_qc'
+	def _basic_info(self):
+		self.basic_stat = 'basic info result'
+		print self.basic_stat
+		""" Basic description of the ChIP-seq  input dataset """
+
+	def _fastqc_info(self):
+		self.has_fastqc = True
+		""" QC analysis of the raw Chip-seq data, including sequence quality score of particularity raw data and the cumulative percentage plot of the sequence quality scores of all historic data.
+		"""
+		print 'fastqc\n'          
+		return 'fastqc_summary','fastqc_pdf'
+
+	def run(self):
+		""" Run some RawQC functions to get final result."""
+		self.basic_stat = self._basic_info()
+		self.fastqc_summary_stat,self.fastqc_graph_stat = self._fastqc_info()
+		self._check()
+		self._render()
+	def _check(self):
+		"""
+		Check whether the FastQC's result is ok
+		"""
+		if self.has_fastqc:
+			print 'input self.fastqc_summary_stat for judge'
+		else:
+			print ' no need fastqc'
+	def _render(self):
+		self.basic_stat = ['111','adasfd','dfsdf','sfdsdf','sdfsd','sdfsdf']
+		self.has_fastqc = ''
+		temp = self.template.render(basic_table = self.basic_stat,fastqc_check = self.has_fastqc,fastqc_graph = self.fastqc_graph_stat)
+		print temp 
+
+
+		
+class MappingQC(QC_Controller):
+	""" MappingQC aims to describe the mapping quality of the sequence alignment. """
+	def __init__(self):
+		super(MappingQC, self).__init__()		
+		print 'init mapping qc'
+	def _basic_mapping_statistics_info(self):
+		""" Stastic summary of mapping result for each sample. """
+		print 'basic_mapping_statistics'
+		""" Cumulative percentage plot to  describe the  mappable ratio quality of all historic data. """
+	def _mappable_ratio_info(self):
+		""" Cumulative percentage plot to  describe the  mappable ratio quality of all historic data."""
+		print 'mappable_ratio'
+	def _redundant_ratio_info(self):
+		""" Show redundant  ratio of the dataset in all historic data"""
+		print 'redundant_ratio\n'
+	def run(self):
+		""" Run some MappingQC function to get final result. """
+		self._basic_mapping_statistics_info()
+		self._mappable_ratio_info()
+		self._redundant_ratio_info()
+	def check():
+		"""Check whether the MappingQC's result is ok. """
+		print 'mapping qc pass or not'
+
+
+class PeakcallingQC(QC_Controller):
+	""" PeakcallingQC aims to describe the quality of peak calling result."""
+	def __init__(self):
+		super(PeakcallingQC, self).__init__()
+		print 'init peak calling  qc'
+	def _peak_summary_info(self):
+		"""Basic statistic of peak calling result."""
+		print ' summary_info'
+
+	def _velcro_ratio_info(self):
+		"""verlcro ratio is used to describe whether the peak is credible , The lower the result is more convenience.
+		 The cumulative percentage plot can reflect the particularly dataset's verlcro ratio quality of all historic data."""
+		print 'velcro_ratio_info '
+
+	def _DHS_ratio_info(self):
+		""" DHS ratio indicate the percentage of peaks overlap with DHSs site.
+		The function can describe  the particularly dataset's DHS ratio quality of all historic data.
+		"""
+		print 'DHS_ratio_info'
+	def _replicate_info(self):
+		""" ReplicateQC aims to describe the similarity of replicate experiment. Venn diagram and correlation plot will be used."""
+		print 'replicate_info\n'
+
+	def run(self):
+		""" Run some PeakcallingQC function to get final result. """
+		self._peak_summary_info()
+		self._velcro_ratio_info()
+		self._DHS_ratio_info()
+		self._replicate_info()
+	def check():
+		""" Check whether PeakcallingQC's result is ok. """
+		print 'pass or not'
+
+		
+class AnnotationQC(QC_Controller):
+	""" AnnotationQC aims to describe the quality of annotations after peak calling. """ 
+	def __init__(self):
+		super(AnnotationQC, self).__init__()
+		print 'intialization of function qc'
+	def _ceas_info(self):
+		""" Describe peaks' distribution and relative position. """
+		print 'ceas qc'
+	def _conservation_info(self):
+		""" Density plot of peaks's conservation."""
+		print 'conservation qc'
+	def _motif_info(self):
+		""" QC of Sepose. """
+		print 'motif info\n'
+	def run(self):
+		""" Run some AnnotationQC function. """
+		self._ceas_info()
+		self._conservation_info()
+		self._motif_info()
+	def check(self):
+		""" Check whether AnnotationQC's result is ok. """
+		print 'pass or not'
+
---
+++ chilin/scripts/ChiLin.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+"""
+Main program of DC pipeline
+"""
+from chilin.dc import *
+from chilin.qc import *
+from optparse import OptionParser
+
+def main():
+    usage = "usage: %prog <ChiLin.conf Path> [optional]-m cormethod -p peaksnumber"
+    description = "ChiLin : A clear ChIP-seq pipeline"
+    parser = OptionParser(version = "%prog 1.0.0", description = description, usage = usage)
+    parser.add_option("-m", dest = "cormethod", type = "string", default = "mean",
+                      help = "specify method for correlation plot")
+    parser.add_option("-p", dest = "peaksnumber", type = "int", default = 3000,
+                      help = "specify peaks number for CEAS, Conservation and options")
+    parser.add_option("-t", dest = "type", type = "string",
+                      help = "specify the analysis type, supported Dnase, Histone, TF")
+    parser.add_option("-s", dest = "shiftsize", type = "string", default= '73',
+                      help = "specify the fixed shiftsize for MACS2")
+#    parser.add_option("-s", dest = "stepcontrol", type = "store_true", default = True)
+    (options, args) = parser.parse_args()
+
+    if not args or not options.type:
+        parser.print_help()
+        sys.exit('options missing')
+
+    ChiLinConf = args[0]
+    Preparation = PipePreparation(ChiLinConf)
+    Preparation.checkconf()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt():
+        print "User stops me:)"
+    finally:
+        print "Welcome to ChiLin"
+
+
+
+
