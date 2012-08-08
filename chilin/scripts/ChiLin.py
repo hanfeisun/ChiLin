4b9a8258eb8bfd2e20b238d56347e509c98c8f25
Hanfei Sun <hfsun.tju@gmail.com>
2012-08-08 13:43:48 -0400

    refactor and remove codes to recognize bed file

--- chilin/scripts/ChiLin.py
+++ chilin/scripts/ChiLin.py
@@ -6,22 +6,35 @@ import sys
 import os
 from pkg_resources import resource_filename
 from optparse import OptionParser
-from chilin.dc import *
-from chilin.qc import *
+
+from chilin.dc import (PipePreparation,
+                       PipeBowtie,
+                       PipeMACS2,
+                       PipeVennCor,
+                       PipeCEAS,
+                       PipeConserv,
+                       PipeMotif,
+                       package)
+
+from chilin.qc import (RawQC,
+                       MappingQC,
+                       PeakcallingQC,
+                       AnnotationQC,
+                       SummaryQC)
 
 def parse_options():
     usage = "usage: %prog <ChiLin.conf Path> [optional]"
     description = "ChiLin : A clear ChIP-seq pipeline"
     parser = OptionParser(version = "%prog 1.0.0", description = description, usage = usage)
-    parser.add_option("-m", dest = "cormethod", type = "string", default = "mean",
+    parser.add_option("-m", dest = "cor_method", type = "string", default = "mean",
                       help = "specify method for correlation plot")
-    parser.add_option("-p", dest = "peaksnumber", type = "string", default = '5000',
+    parser.add_option("-p", dest = "top_peaks", type = "string", default = '5000',
                       help = "specify peaks number for CEAS")
     parser.add_option("-t", dest = "atype", type = "string",
                       help = "It is a most important option for ChiLin specify the analysis type, supported Dnase, Histone, TF")
     parser.add_option("-s", dest = "shiftsize", type = "string", default= '73',
                       help = "specify the fixed shiftsize for MACS2, advice Dnase: 50, Histone and TF:73")
-    parser.add_option("-c", dest = "stepcontrol", type = "int", default = 6,
+    parser.add_option("-d", dest = "step_end", type = "int", default = 6,
                       help = "specify the end step of pipeline, 1 for bowtie, 2 for macs, 3 for venn and correlation, 4 for ceas, 5 for conservation, 6 for motif, Note: if you only have bed file, start from 2")
     parser.add_option("-e", action="store_true", dest="example_conf", default=False,
                       help = "Show an example of configuration file")
@@ -31,9 +44,14 @@ def parse_options():
         with open(resource_filename("chilin", os.path.join("db", "ChiLin_human.conf"))) as f:
             print f.read()
         sys.exit(1)
-    if not args or not options.atype or options.atype not in ['TF', 'Dnase', 'Histone']:
+    if not args:
         parser.print_help()
         sys.exit(1)
+    if not options.atype :
+        parser.print_help()
+        print "Please select the analysis type (Dnase, Histone or TF)!"
+        sys.exit(1)
+        
     if options.atype in ['TF', 'Histone']:
         options.shiftsize = '73'
     elif options.atype == 'Dnase':
@@ -41,43 +59,65 @@ def parse_options():
     return options, args
 
 def main():
-    options, args = parse_options()
-    ChiLinConf = args[0]
-    Preparation = PipePreparation(ChiLinConf)
-    bedft = Preparation.checkconf()
-    conf = Preparation.ChiLinconfigs
-    names = Preparation.Nameconfigs
-    log = Preparation.log
-    s = options.stepcontrol
-    p = options.peaksnumber
-    m = options.cormethod
-    datasummary = Preparation.DataSummary
+    opt, args = parse_options()
+    conf_file = args[0]
+    
+    pp = PipePreparation(conf_file)
+    log_func = pp.func_log()
 
-    texfile,summarycheck = QC_Controller().QCpreparation(names)
-    rawqc = RawQC(conf,names,texfile,summarycheck,log)
-    rawqc.run()
+    pp.check_conf()
+    conf = pp.get_config()
+    rule = pp.get_rule()
 
-#    PipeBowtie(conf, names, log, datasummary, s, bedft).process()
-    mappingqc = MappingQC(conf,names,texfile,rawqc.summarycheck,log)
-    mappingqc.run(bedft)
+    with open(pp.get_tex(),"w") as f_tex:
+        with open(pp.get_summary(),"w") as f_sum:
+            rawqc = RawQC(conf = conf, rule = rule,
+                          texfile = f_tex, log = log_func)
+            rawqc.run()
 
-#    macs2 = PipeMACS2(conf, names, log, datasummary, s, options.shiftsize,bedft)
-#    macs2.process()
+            pipebowtie = PipeBowtie(conf = conf, rule = rule, log = log_func,
+                                datasummary= f_sum, stepcontrol = opt.step_end)
+            pipebowtie.run()
 
-#    PipeVennCor(conf, names, log, datasummary, s, macs2.rendercontent, p, m).process()
-    peakcallingqc = PeakcallingQC(conf,names,texfile,mappingqc.summarycheck,log)
-    peakcallingqc.run()
-
-#    PipeCEAS(conf, names, log, s, p).process()
-#    PipeConserv(conf, names, options.atype, log, s).process()
-#    PipeMotif(conf, names, log, s).process()
-    print 'ceas'
-    annotationqc = AnnotationQC(conf,names,texfile,peakcallingqc.summarycheck,log)
-    annotationqc.run()
-    SummaryQC(conf,names,texfile).run(summarycheck)
-
-#    package(conf, names, log)
-
+            mappingqc = MappingQC(conf = conf, rule = rule, log = log_func,
+                                  texfile = f_tex, summarycheck = rawqc.summarycheck)
+            mappingqc.run(bedft)
+            
+            macs2 = PipeMACS2(conf = conf, rule = rule, log = log_func,
+                              datasummary = f_sum, stepcontrol = opt.step_end,
+                              shiftsize = opt.shiftsize, bedft = bedft)
+            macs2.run()
+            
+            pipevenncor = PipeVennCor(conf = conf, rule = rule, log = log_func,
+                                      datasummary = f_sum, stepcontrol = opt.step_end,
+                                      ratios = macs2.rendercontent, peaksnumber = opt.top_peaks,
+                                      OptionMethod = opt.cor_method)
+            pipevenncor.run()
+            
+            peakcallingqc = PeakcallingQC(conf = conf,rule =rule, log = log_func,
+                                          texfile = f_tex, summarycheck = mappingqc.sum_check)
+            peakcallingqc.run()
+            
+            pipeceas = PipeCEAS(conf = conf, rule = rule, log = log_func,
+                                stepcontrol = opt.step_end, peaksnumber = p)
+            pipeceas.run()
+            
+            pipeconserv = PipeConserv(conf = conf, rule = rule, log = log_func,
+                                      stepcontrol = opt.step_end, a_type = opt.atype)
+            pipeconserv.run()
+            
+            pipemotif = PipeMotif(conf = conf, rule = rule, log = log_func,
+                                  stepcontrol = opt.step_end)
+            pipemotif.run()
+            
+            annotationqc = AnnotationQC(conf = conf, rule = rule, log = log_func,
+                                        texfile = f_tex, summarycheck = peakcallingqc.sum_check)
+            annotationqc.run()
+            
+            summaryqc = SummaryQC(conf = conf, rule = rule, log = log_func,
+                                  texfile = f_tex)
+            summaryqc.run(sum_check)
+            
 
 
 if __name__ == "__main__":
