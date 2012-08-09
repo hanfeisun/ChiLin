92edeaeaae716f62638ac0893b486a56e45199fc
Hanfei Sun <ad9075@gmail.com>
2012-08-09 19:08:17 -0400

    Improve debug mode Add threads control

--- chilin/scripts/ChiLin.py
+++ chilin/scripts/ChiLin.py
@@ -5,8 +5,8 @@ Main program of DC pipeline
 import sys
 import os
 from pkg_resources import resource_filename
-from optparse import OptionParser
-
+import argparse
+from functools import partial
 from chilin.dc import (PipePreparation,
                        PipeBowtie,
                        PipeMACS2,
@@ -22,100 +22,104 @@ from chilin.qc import (RawQC,
                        AnnotationQC,
                        SummaryQC)
 
-def parse_options():
-    usage = "usage: %prog <ChiLin.conf Path> [optional]"
-    description = "ChiLin : A clear ChIP-seq pipeline"
-    parser = OptionParser(version = "%prog 1.0.0", description = description, usage = usage)
-    parser.add_option("-m", dest = "cor_method", type = "string", default = "mean",
-                      help = "specify method for correlation plot")
-    parser.add_option("-p", dest = "top_peaks", type = "string", default = '5000',
-                      help = "specify peaks number for CEAS")
-    parser.add_option("-t", dest = "atype", type = "string",
-                      help = "It is a most important option for ChiLin specify the analysis type, supported Dnase, Histone, TF")
-    parser.add_option("-s", dest = "shiftsize", type = "string", default= '73',
-                      help = "specify the fixed shiftsize for MACS2, advice Dnase: 50, Histone and TF:73")
-    parser.add_option("-d", dest = "step_end", type = "int", default = 6,
-                      help = "specify the end step of pipeline, 1 for bowtie, 2 for macs, 3 for venn and correlation, 4 for ceas, 5 for conservation, 6 for motif, Note: if you only have bed file, start from 2")
-    parser.add_option("-e", action="store_true", dest="example_conf", default=False,
-                      help = "Show an example of configuration file")
-    (options, args) = parser.parse_args()
+class ChiLinParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit()
 
-    if options.example_conf:
-        with open(resource_filename("chilin", os.path.join("db", "ChiLin_human.conf"))) as f:
+
+def parse_args():
+    description = "ChiLin : A clear ChIP-seq pipeline"
+    parser = ChiLinParser(description = description)
+    sub_parsers = parser.add_subparsers(help = "sub-command help", dest = "sub_command")
+    
+    template_parser = sub_parsers.add_parser("gen",  help = "generate a template of config file",
+                                             description = "ChiLin-gen: A config template generator for ChiLin")
+    template_parser.add_argument("--species", choices = ("hg19", "mm9"), required = True)
+    
+    pipe_parser = sub_parsers.add_parser("run", help = "run pipeline using a config file",
+                                         description = "ChiLin-run: Run ChiLin pipeline using a config file")
+    pipe_parser.add_argument("-c","--config", required = True,
+                        help = "specify the config file to use", )
+    pipe_parser.add_argument("-t", dest = "atype", choices = ("Dnase", "Histone", "TF"), required = True,
+                        help = "the most important option for ChiLin specify the analysis type and the shiftsize {Dnase: 50, Histone and TF:73} of MACS2")
+    pipe_parser.add_argument("-m", dest = "cor_method", default = "mean",choices = ("mean",),
+                        help = "specify method for correlation plot")
+    pipe_parser.add_argument("-p", dest = "top_peaks", type = int, default = 5000,
+                        help = "specify peaks number for CEAS")
+
+    pipe_parser.add_argument("--threads", dest = "max_threads", type = int, default = 1, choices = range(1,9),
+                             help = "How many threads can be used")
+
+    pipe_parser.add_argument("-d", dest = "step_end", type = int, default = 6,
+                        help = "specify the end step of pipeline, 1 for bowtie, 2 for macs, 3 for venn and correlation, 4 for ceas, 5 for conservation, 6 for motif, Note: if you only have bed file, start from 2")
+    pipe_parser.add_argument("--debug", help = "debug mode", action = "store_true", default = False)
+    
+    args = parser.parse_args()
+
+
+    if args.sub_command == "gen":
+        db = {"hg19": "ChiLin_human.conf", "mm9": "ChiLin_mouse.conf"}
+        with open(resource_filename("chilin", os.path.join("db", db[args.species]))) as f:
             print f.read()
-        sys.exit(1)
-    if not args:
-        parser.print_help()
-        sys.exit(1)
-    if not options.atype :
-        parser.print_help()
-        print "Please select the analysis type (Dnase, Histone or TF)!"
-        sys.exit(1)
-        
-    if options.atype in ['TF', 'Histone']:
-        options.shiftsize = '73'
-    elif options.atype == 'Dnase':
-        options.shiftsize = '50'
-    return options, args
+        sys.exit(0)
+
+    if args.sub_command == "run":
+        shift = {"TF": 73, "Histone": 73, "Dnase": 50}
+        args.shiftsize = shift[args.atype]
+    return args
 
 def main():
-    opt, args = parse_options()
-    conf_file = args[0]
+    args = parse_args()
     
-    pp = PipePreparation(conf_file)
+    pp = PipePreparation(args.config)
     log_func = pp.func_log()
 
     pp.check_conf()
+
     conf = pp.get_config()
     rule = pp.get_rule()
 
+    print conf
     with open(pp.get_tex(),"w") as f_tex:
         with open(pp.get_summary(),"w") as f_sum:
-            rawqc = RawQC(conf = conf, rule = rule,
-                          texfile = f_tex, log = log_func)
+            p = lambda func:partial(func, conf=conf, rule=rule, log=log_func,
+                                    debug=args.debug, texfile=f_tex,
+                                    datasummary = f_sum,
+                                    stepcontrol=args.step_end, a_type = args.atype,
+                                    shiftsize = args.shiftsize,
+                                    peaksnumber = args.top_peaks,
+                                    ArgsionMethod = args.cor_method,
+                                    threads = args.max_threads)
+
+            rawqc = p(RawQC)()
             rawqc.run()
-
-            pipebowtie = PipeBowtie(conf = conf, rule = rule, log = log_func,
-                                datasummary= f_sum, stepcontrol = opt.step_end)
-            pipebowtie.run()
-
-            mappingqc = MappingQC(conf = conf, rule = rule, log = log_func,
-                                  texfile = f_tex, summarycheck = rawqc.summarycheck)
+            mappingqc = p(MappingQC)(summarycheck = rawqc.summarycheck)
             mappingqc.run()
             
-            macs2 = PipeMACS2(conf = conf, rule = rule, log = log_func,
-                              datasummary = f_sum, stepcontrol = opt.step_end,
-                              shiftsize = opt.shiftsize)
+            macs2 = p(PipeMACS2)()
             macs2.run()
             
-            pipevenncor = PipeVennCor(conf = conf, rule = rule, log = log_func,
-                                      datasummary = f_sum, stepcontrol = opt.step_end,
-                                      ratios = macs2.rendercontent, peaksnumber = opt.top_peaks,
-                                      OptionMethod = opt.cor_method)
+            pipevenncor = p(PipeVennCor)(ratios = macs2.rendercontent)
             pipevenncor.run()
             
-            peakcallingqc = PeakcallingQC(conf = conf,rule =rule, log = log_func,
-                                          texfile = f_tex, summarycheck = mappingqc.sum_check)
+            peakcallingqc = p(PeakcallingQC)(summarycheck = mappingqc.summarycheck)
             peakcallingqc.run()
             
-            pipeceas = PipeCEAS(conf = conf, rule = rule, log = log_func,
-                                stepcontrol = opt.step_end, peaksnumber = p)
+            pipeceas = p(PipeCEAS)()
             pipeceas.run()
             
-            pipeconserv = PipeConserv(conf = conf, rule = rule, log = log_func,
-                                      stepcontrol = opt.step_end, a_type = opt.atype)
+            pipeconserv = p(PipeConserv)()
             pipeconserv.run()
             
-            pipemotif = PipeMotif(conf = conf, rule = rule, log = log_func,
-                                  stepcontrol = opt.step_end)
+            pipemotif = p(PipeMotif)()
             pipemotif.run()
             
-            annotationqc = AnnotationQC(conf = conf, rule = rule, log = log_func,
-                                        texfile = f_tex, summarycheck = peakcallingqc.sum_check)
+            annotationqc = p(AnnotationQC)(summarycheck = peakcallingqc.summarycheck)
             annotationqc.run()
             
-            summaryqc = SummaryQC(conf = conf, rule = rule, log = log_func,
-                                  texfile = f_tex)
+            summaryqc = p(SummaryQC)()
             summaryqc.run(sum_check)
             
 
