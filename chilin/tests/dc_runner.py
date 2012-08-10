16b258d80806593221be357acf50a59a076b208f
Alvin_Qin <qinqianhappy@gmail.com>
2012-08-10 21:19:16 +0800

    add gen conf template

---
+++ chilin/tests/dc_runner.py
@@ -0,0 +1,12 @@
+from chilin.dc import (
+    Check,
+    TemplateParser,
+    Log,
+    DcController,
+    Bowtie,
+    MACS,
+    Replicates,
+    CEAS,
+    Seqpos,
+    Conserv)
+import unittest
