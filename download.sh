c267e80c1a01ed6eb8f60953284942a9b06d7eb4
qinqian <qinqianhappy@gmail.com>
2012-09-20 15:10:38 +0800

    modify download.sh and readme

--- download.sh
+++ download.sh
@@ -3,32 +3,23 @@
 # and update the original ChiLin config
 # at the same time check all dependency
 #------------------------
-# # chilin source
-# get https://bitbucket.org/shenglinmei/chilin/get/6e472d7e69a6.tar.gz
-# tar xvfz *.tar.gz
-# cd shenglinmei*
-# python setup.py install
-# bam2fastx not yet
-
 help()
 {
     cat <<HELP
 usage: $0 install [-p DIR]
-
 optional arguments:
   -p DIR		specify the directory to store the downloaded data DEFAULT: DOWNLOADED
-
 HELP
-
 }
 
+# options
 while [ -n "$1" ]; do
     case $1 in
-	-h)help;shift 1;;
-	-p)pathd=$2; shift 2;;
-	--)shift; break;; # options end
-	-*) echo "error: no such option $1. -h for help";exit 1;;
-	*) break;;
+        -h)help;shift 1;;
+        -p)pathd=$2; shift 2;;
+        --)shift; break;; # options end
+        -*) echo "error: no such option $1. -h for help";exit 1;;
+        *) break;; 
     esac
 done
 
@@ -36,28 +27,34 @@ if [ -n $pathd ]; then
     help
     echo "Please input the path for storing download data"
 fi
+if [ -d $pathd ]
+then
+    echo "directory exists"
+else
+    mkdir $pathd
+fi
 
 ## choose for all users or single user
-#PS3="Choose (1-2)"
-#echo "Choose from the list below."
-#select name in global personal
-#do
-#    break
-#done
-#if [ $name = global ]; then
+PS3="Choose (1-2)"
+echo "Choose from the list below."
+select name in global personal
+do
+   break
+done
+
+if [ $name = global ]; then
 u=`whoami`
 if [ $u = root ]; then
     echo "install for all users"
-else
+elif
     echo "Need root authority, please ask your administrator!"
     exit 1
+else
+   echo "Please reinput your ChiLin data to $pathd for single user"
 fi
-#else
-#    echo "get ChiLin data to $pathd for single user"
-#fi
-
 # Users could customize executive bin path
 read -p "choose where the bin is " bin
+read -p "choose where the data is" data
 if [ ! -n $bin ];then
     if (echo $PATH|grep $bin) ; then echo 1;
     else
@@ -65,37 +62,27 @@ if [ ! -n $bin ];then
         bin=/usr/bin
     fi
 fi
-
-# TODO: User could also customize data path
-
-cp chilin/lib/db/*.bed $bin
-cp chilin/lib/db/*.txt $bin
-unzip chilin/lib/db/DHS.zip -d $bin
-
-
-if [ -d $pathd ]
-then
-    echo "directory exists"
-else
-    mkdir $pathd
+if [ ! -f $data ]; then
+    mkdir $data
 fi
+
+# cp built-in data
+cp chilin/lib/db/*.bed $data
+cp chilin/lib/db/*.txt $data
+unzip chilin/lib/db/DHS.zip -d $data
+
+# change into pathd to download and install
 cd $pathd
 
 # for judging machine type for download the right
 # source or binary
 if uname -a | grep Darwin; then
-#    if uname -a | grep x86; then
-#        machine="macOSX.ppc"
-##	echo "Your machine type is 64 mac machine"
-#    else
     machine="macOSX.i386"
-#        echo "Your machine type is 32 mac machine"
     echo "your machine is mac"
-#    fi
 elif uname -a |grep Linux; then
     if uname -a | grep x86; then
         machine="linux.x86_64"
-	echo "Your machine type is linux machine"
+        echo "Your machine type is linux.x86_64"
     else
         machine="Your machine type is linux.i386"
     fi
@@ -104,45 +91,61 @@ fi
 get() {
     file=$1
     if ! wget --version >/dev/null 2>/dev/null ; then
-	if ! curl --version >/dev/null 2>/dev/null ; then
-	    echo "Please install wget or curl somewhere in your PATH"
-	    exit 1
-	fi
-	curl -o `basename $1` -L $1
-	return $?
+        if ! curl --version >/dev/null 2>/dev/null ; then
+            echo "Please install wget or curl somewhere in your PATH"
+            exit 1
+        fi
+        curl -o $2 -L $1
+        return $?
     else
-	wget $1 -O `basename $1`
-	return $?
+        wget $1 -O $2
+        return $?
     fi
 }
 
 install() {
-    # TODO: detect whether python version is 2.7
     suffix=$1
     if [ ${suffix##*.} = py ];then
-	echo "installining $2"
-	python setup.py install --prefix=`dirname $bin`
-	return $?
+        if python -V 2>&1 | grep 2.7; then
+            echo "installining $2"
+            python setup.py install --prefix=`dirname $bin`
+        else
+            echo "Please install python 2.7 version"
+            return $?
+        fi
     elif [ ${suffix#*.} = cc ]; then
         name=`echo $2|cut -d . -f 1`
-	cd $name-*
-	make
-	if [ -d bin ]; then
-	    cp -r bin/* $bin
-	else
-	    cp $name $bin
-	fi
-	cd -
-	return $?
+        cd $name-*
+        make
+        if [ -d bin ]; then
+            cp -r bin/* $bin
+        else
+            cp $name $bin
+        fi
+        cd -
+        return $?
     else
-	cp `basename $2` $bin
-	return $?
+        cp `basename $2` $bin
+        return $?
     fi
 }
 
-# depend source code and binary
-# machine type specific 
-# mac_ppc version test failure, use i386
+# depend data
+# bowtie index
+species="\
+mm9 \
+hg19 \
+"
+BowtieIndexBase="ftp://ftp.cbcb.umd.edu/pub/data/bowtie_indexes/"
+for index in $species; do
+    if [ ! -f ${index}.ebwt.zip ]; then
+        if get ${BowtieIndexBase}/${index}.ebwt.zip ${index}.ebwt.zip; then
+            echo "Bowtie index download successfully"
+            unzip ${BowtieIndexBase}/${index}.ebwt.zip -d $data
+        fi
+    fi
+done
+# depend software
 URL_BASE="\
 http://bedtools.googlecode.com/files/BEDTools.v2.16.2.tar.gz \
 http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.10.1.zip \
@@ -151,12 +154,20 @@ http://hgdownload.cse.ucsc.edu/admin/exe/$machine/bedGraphToBigWig \
 "
 for i in $URL_BASE;do
     if [ ! -f `basename $i` ]; then
-        if get $i; then
+        if get $i `basename $i`; then
             echo "download $i ready"
         fi
     fi
 done
+# sourceforge special
+if [ ! -f bowtie.zip ]; then
+    get http://sourceforge.net/projects/bowtie-bio/files/bowtie/0.12.8/bowtie-0.12.8-src.zip/download bowtie.zip
+    if [ ! -f samtools.tar.bz2 ]; then
+        get http://sourceforge.net/projects/samtools/files/samtools/0.1.18/samtools-0.1.18.tar.bz2/download  samtools.tar.bz2
+    fi
+fi
 
+# install
 install other bedClip
 install other bedGraphToBigWig
 unzip fastqc_v0.10.1.zip -d $bin >/dev/null
@@ -164,38 +175,12 @@ cd $bin
 chmod 755 $bin/FastQC/fastqc
 cd -
 
-## for sourceforge special
-if [ ! -f bowtie.zip ]; then
-    wget -c http://sourceforge.net/projects/bowtie-bio/files/bowtie/0.12.8/bowtie-0.12.8-src.zip/download -O bowtie.zip
-    if [ ! -f samtools.tar.bz2 ]; then
-	wget -c http://sourceforge.net/projects/samtools/files/samtools/0.1.18/samtools-0.1.18.tar.bz2/download -O samtools.tar.bz2
-    fi
-    # TODO: curl implementation
-fi
-
 unzip bowtie.zip >/dev/null
 echo "install bowtie"
 install cc bowtie.zip
 tar xvfj samtools.tar.bz2 >/dev/null 2>&1
 install cc samtools.tar.bz2
 echo "install samtools"
-
-# bowtie index
-# you may add more index
-species="\
-mm9 \
-hg19 \
-"
-BowtieIndexBase="ftp://ftp.cbcb.umd.edu/pub/data/bowtie_indexes/"
-for index in $species; do
-    if [ ! -f ${index}.ebwt.zip ]; then
-	if get ${BowtieIndexBase}/${index}.ebwt.zip; then
-	    echo "Bowtie index download successfully"
-	    unzip ${BowtieIndexBase}/${index}.ebwt.zip -d $bin
-	    # Use data instead of bin
-	fi
-    fi
-done
 
 # github certificate macs2
 if type -a macs2
@@ -222,6 +207,7 @@ if [ ! -f cistrome-app.tar.gz ]; then
 fi
 tar xvfz cistrome-app.tar.gz >/dev/null 2>&1
 cd cistrome-cistrome-applications-harvard*
+
 # ceas
 cd published-packages/CEAS
 install py CEAS
@@ -230,13 +216,12 @@ cp -rf *.refGene $bin
 cd ../../../cistrome-extra-apps built-ins
 install py built-ins
 
-# mdseqpos problem: gcc setting and settings.py
-# 
+# mdseqpos 
 export CC=gcc  # upper case
 cd ../mdseqpos
 cp lib/settings.py.example lib/settings.py
 install py mdseqpos
-#
+
 # special download need copy sub directory
 # conservation Phascon bigwiggle
 BASE_CHRS="\
@@ -267,13 +252,11 @@ chrY \
 chrM"
 for s in $species;do
     for c in $BASE_CHRS;do
-	mkdir conservation_$s 2>/dev/null
-	wget -c http://cistrome.org/~hanfei/conservation/$s/vertebrate/${c}.bw -O conservation_$s/${c}.bw
+        mkdir conservation_$s 2>/dev/null
+        wget -c http://cistrome.org/~hanfei/conservation/$s/vertebrate/${c}.bw -O conservation_$s/${c}.bw
     done
-    cp -r conservation_$s $bin
+    cp -r conservation_$s $data
 done
-
-cd ../../.. #back to the chilin root directory
 
 echo "\
 [UserInfo]
@@ -289,14 +272,15 @@ OutputDirectory =
 #BAMTOFASTQ = $bin/bam2fastx
 # latest bedtools
 BAMTOFASTQ = $bin/bamToFastq
+
 BOWTIE_MAIN = $bin/bowtie
 BOWTIE_GENOME_INDEX_PATH = $bin/{{ species }}
 nBOWTIE_MAX_ALIGNMENT = 1
 
 [samtools]
 SAMTOOLS_MAIN = $bin/samtools
-SAMTOOLS_CHROM_LEN_PATH = $bin/chromInfo_{{ species }}.txt
-CHROM_LEN_BED_PATH = $bin/chr_limit_{{ species }}.bed
+SAMTOOLS_CHROM_LEN_PATH = $data/chromInfo_{{ species }}.txt
+CHROM_LEN_BED_PATH = $data/chr_limit_{{ species }}.bed
 
 [macs]
 MACS_MAIN = $bin/macs2
@@ -312,8 +296,8 @@ BEDTOBAM_MAIN = $bin/bedToBam
 [ceas]
 CEAS_MAIN = $bin/ceasBW
 ceas_ex = $bin/ceas-exon
-CEAS_GENETABLE_PATH = $bin/{{ species }}.refGene
-CHROM_LEN = $bin/chromInfo_{{ species }}.txt
+CEAS_GENETABLE_PATH = $data/{{ species }}.refGene
+CHROM_LEN = $data/chromInfo_{{ species }}.txt
 CEAS_PROMOTER_SIZES = 
 CEAS_BIPROMOTER_SIZES = 
 CEAS_REL_DIST = 
@@ -333,8 +317,8 @@ WIG_CORRELATION_MAX = 50
 
 [venn]
 VENN_DIAGRAM_MAIN = $bin/venn_diagram.py
-DHS_BED_PATH = $bin/DHS/DHS_{{ species }}.bed 
-VELCRO_PATH = $bin/wgEncodeHg19ConsensusSignalArtifactRegions.bed
+DHS_BED_PATH = $data/DHS/DHS_{{ species }}.bed 
+VELCRO_PATH = $data/wgEncodeHg19ConsensusSignalArtifactRegions.bed
 # velcro only available for hg19
 
 [seqpos]
@@ -351,14 +335,18 @@ SEQPOS_MOTIF_DB_SELECTION = cistrome.xml
 FASTQC_MAIN = $bin/FastQC/fastqc
 FILTERDUP_SPECIES = {{ filterdup_species }}
 " > chilin/lib/template/ChiLinjinja.conf.new
+if [ $? -eq 0 ]; then
+    echo "Now automatic  generated conf is chilin/lib/template/ChiLinjinja.conf.new"
+    echo "template conf has been replaced by the new conf"
+    cp chilin/lib/template/ChiLinjinja.conf.new chilin/lib/template/ChiLinjinja.conf
+fi
 
-# TODO: display informatrion that the new file is chilin/lib/template/ChiLinjinja.conf.new
-# TODO: copy the ChiLinjinja.conf.new to ChiLinjinja.conf
-
+# install Chilin
+cd ../../.. #back to the chilin root directory
 echo "========================================="
-#python setup.py install # ChiLin
-
 if [ $? -eq 0 ]; then
     echo "ChiLin data config succeed!:)"
 fi
 echo "Please run ChiLin setup.py"
+
+
