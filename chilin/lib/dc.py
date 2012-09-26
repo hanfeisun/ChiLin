25fb88cc9cd16316980556a8b6aec371c94ab052
Alvin_Qin <qinqianhappy@gmail.com>
2012-09-26 22:26:32 +0800

    debug awk

--- chilin/lib/dc.py
+++ chilin/lib/dc.py
@@ -378,20 +378,19 @@ class PipeBowtie(PipeController):
             BEGIN{total=0; map=0; a=0;b=0}
             {
             if (/^[^@]/){
-                total+=1 {
-                    if ($2!="4") {
-                        map+=1 
-                        ur[$1] += 1
-                        ul[$2":"$3":"$4] += 1
-                    }
-            }
+                total+=1 
+                if ($2!="4") {
+                    map+=1 
+                    ur[$1] += 1
+                    ul[$2":"$3":"$4] += 1
+                }
             }
             }
             END{
-                for (urr in ur)
-                    a+=1
-                for (ull in ul)
-                    b+=1
+            for (urr in ur)
+                a+=1
+            for (ull in ul)
+                b+=1
             print total
             print map
             print a
